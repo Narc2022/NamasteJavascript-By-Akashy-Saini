122b8c2c747bca945401696a81eb31fd41346daf
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2022-05-27 16:03:18 +0530

    SCOPE

--- lecture6/undefine.js
+++ lecture6/undefine.js
@@ -1,1 +1,18 @@
-var a=7;
+
+// var a=10;
+// console.log(a);
+
+// if (a===undefined){
+//     console.log("a is undefined");
+// }else{
+//     console.log("a is not undefined");
+// }
+
+/**********************Javascript is loosly typed language/flexible**********************/
+
+var a;
+console.log(a);
+a=10;
+console.log(a);
+a="hello world";
+console.log(a);
---
+++ lecture7/scope.js
@@ -0,0 +1,22 @@
+/*function a() {
+    c();
+    function c() {
+        console.log(b);
+    }
+}
+
+a();*/
+
+
+/**example unable to access var outside the scope**/
+
+
+/*function a() {
+    var b = 10;
+    c();
+    function c() {
+        
+    }
+}
+console.log(b);
+a();*/
